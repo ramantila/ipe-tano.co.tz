498f7b7ad0f3db4e2fad4731b2e76bd9014ef218
NuhuWambali <wambalinuhu@gmail.com>
2025-02-14 04:20:45 -0800

    category implemented successfully

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace App\Http\Controllers;
@@ -368,3 +369,375 @@ class ConsumerController extends Controller
 
 
 }
+=======
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\RegisterMail;
+use App\Models\Business;
+use App\Models\BusinessReview;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductReview;
+use App\Models\Region;
+use App\Models\Service;
+use App\Models\ServiceReview;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use Illuminate\Support\Facades\Mail;
+use Session;
+
+class ConsumerController extends Controller
+{
+    public function index()
+    {
+    
+        $topCompanies = Business::where('total_rating', '>', 2)
+        ->where('total_rating', '<=', 5)
+        ->take(3) 
+        ->get();
+        
+        $categories = Category::take(6)->get();
+
+        return view('.consumer.themes.index', compact('topCompanies', 'categories'));
+    }
+
+    public function indexBusiness()
+    {
+
+        return view('business_fontend.index');
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        $business = Business::where('business_name', 'like', '%' . $query . '%')
+            ->get();
+
+        $category = Category::where('category_name', 'like', '%' . $query . '%')
+            ->get();
+
+        $results = [
+            'business' => $business,
+            'category' => $category,
+        ];
+
+        return response()->json($results);
+    }
+
+    public function searchProduct(Request $request)
+    {
+        $query = $request->input('query');
+
+        $product = Product::where('product_name', 'like', '%' . $query . '%')
+        ->with('business')  // Eager load the related business
+        ->get()
+        ->map(function ($product) {
+            // Add business_name to each product
+            $product->business_name = $product->business ? $product->business->business_name : null;
+            return $product;
+        });
+
+    // Search for services and eager load business name
+    $service = Service::where('service_name', 'like', '%' . $query . '%')
+        ->with('business')  // Eager load the related business
+        ->get()
+        ->map(function ($service) {
+            // Add business_name to each service
+            $service->business_name = $service->business ? $service->business->business_name : null;
+            return $service;
+        });
+
+        $results = [
+            'product' => $product,
+            'service' => $service,
+        ];
+
+        return response()->json($results);
+    }
+
+    public function reviews(Request $request)
+    {
+
+        $categories = Category::orderBy('id', 'DESC')->get();
+
+        $search = $request->input('search');
+        $businesses = Business::when($search, function($query) use ($search) {
+            return $query->where('business_name', 'like', '%'.$search.'%')
+                         ->orWhere('description', 'like', '%'.$search.'%');
+        })
+        ->orderBy('id', 'DESC')
+        ->paginate(10);
+        $bestCompanies = Business::take(15)->orderBy('total_rating','DESC')->get(['business_name', 'logo','id']);
+        $topCategories = Category::take(15)->get(['category_name','category_icon']);
+      
+
+
+        return view('consumer.themes.reviews.reviews', compact('businesses', 'categories','search','bestCompanies','topCategories'));
+    }
+
+    public function reviewsCompany(Request $request)
+    {
+
+        $categories = Category::orderBy('id', 'DESC')->get();
+
+        $businesses = Business::where('business_name', 'like', '%' . $request->company . '%')->orderBy('id', 'DESC')->get();
+
+        return view('consumer.themes.reviews.review_company', compact('businesses', 'categories'));
+    }
+
+    public function reviewsCompanyCategory(Request $request)
+    {
+
+        if ($request->categoryId == 'all') {
+
+            $categories = Category::orderBy('id', 'DESC')->get();
+
+            $businesses = Business::orderBy('id', 'DESC')->get();
+        } else {
+            $category = Category::where('category_name', $request->categoryName)->first();
+
+            $categories = Category::orderBy('id', 'DESC')->get();
+
+            $businesses = Business::where('category_id', $category->id)->orderBy('id', 'DESC')->get();
+        }
+
+        return view('consumer.themes.reviews.category',  compact('businesses', 'categories'));
+    }
+
+    public function review($business_id)
+    {
+
+        $business = Business::with('products', 'services', 'reviews')->find($business_id);
+        return view('consumer.themes.reviews.review', compact('business'));
+    }
+
+    public function businessReviewReported($business_id)
+    {
+
+        return view('consumer.themes.review_report.business_review_report', compact('business_id'));
+    }
+
+    public function storebusinessReviewReported(Request $request, $business_id)
+    {
+
+        $business = BusinessReview::find($business_id);
+        $business->review_reason = $request->reason;
+        $business->status = 2;
+        $business->save();
+
+        Session::flash('success', 'Report sent successfull!');
+        return redirect('business/reviews/' . $business->business_id);
+    }
+
+    public function categories()
+    {
+
+        $data  = Category::orderBy('id', 'DESC')->get();
+
+        return view('consumer.themes.categories', compact('data'));
+    }
+
+    public function categoryBusinesses($category_name)
+    {
+
+        $data = Category::orderBy('id', 'DESC')->get();
+
+        // $categoryCompany = Category::where('category_name', $category_name)->first()->business;
+        $categoryCompany = Category::where('category_name', $category_name)
+            ->with(['business' => function ($query) {
+                $query->where('display', 1);
+                $query->where('status', 1);
+            }])
+            ->first();
+
+        $categories = Category::take(10)->get();
+
+        return view('consumer.themes.category_companies', compact('data', 'categoryCompany', 'categories'));
+    }
+
+    public function terms()
+    {
+        return view('consumer.user.terms');
+    }
+
+    public function register()
+    {
+
+        $regions = Region::get();
+
+        return view('consumer.user.register', compact('regions'));
+    }
+
+    public function registerProcess(Request $request)
+    {
+
+        $phone = $request->phone;
+
+        // Check if the number starts with '255'
+        if (substr($phone, 0, 3) === '255') {
+            // Remove '255' and add '0' at the beginning
+            $phone = '0' . substr($phone, 3);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'phone' => $phone,
+            'password' => $request->password,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'region_id' => $request->region_id,
+            'gender' => $request->gender,
+            'dob' => $request->dob,
+        ];
+
+        $user = Sentinel::registerAndActivate($credentials);
+        $role = Sentinel::findRoleByName('consumer');
+        $role->users()->attach($user->id);
+
+        $credentials = array(
+            "email" => $request->email,
+            "password" => $request->password,
+        );
+
+        $user = Sentinel::authenticate($credentials);
+
+        Mail::to($user->email)->send(new RegisterMail());
+
+        Session::flash('success', 'Registered  successfull!');
+
+        return redirect('/login');
+
+        // if ($user)
+        // {
+        //     // Mail::to($user->email)->send(new RegisterMail());
+
+        //     if(Session::get('type') == 'business')
+        //     {
+        //         return redirect('consumer/business/write-review/'.Session::get('company_id'));
+        //     }
+        //     if(Session::get('type') == 'product')
+        //     {
+        //         return redirect('consumer/product/write-review/'.Session::get('product_id'));
+        //     }
+        //     if(Session::get('type') == 'service')
+        //     {
+        //         return redirect('consumer/service/write-review/'.Session::get('service_id'));
+        //     }
+        // }
+
+        // Session::flash('success', 'Registered  successfull!');
+        // return redirect('/login');
+
+    }
+
+    public function productReviewReported($review_id)
+    {
+
+        return view('consumer.themes.review_report.product_review_report', compact('review_id'));
+    }
+
+    public function storeproductReviewReported(Request $request, $review_id)
+    {
+
+        $product = ProductReview::find($review_id);
+        $product->review_reason = $request->reason;
+        $product->status = 2;
+        $product->save();
+
+        Session::flash('success', 'Report sent successfull!');
+        return redirect('consumer/product/read-more/' . $product->product_id);
+    }
+
+    public function serviceReviewReported($review_id)
+    {
+
+        return view('consumer.themes.review_report.service_review_report', compact('review_id'));
+    }
+
+    public function storeserviceReviewReported(Request $request, $review_id)
+    {
+
+        $service = ServiceReview::find($review_id);
+        $service->review_reason = $request->reason;
+        $service->status = 2;
+        $service->save();
+
+        Session::flash('success', 'Report sent successfull!');
+        return redirect('consumer/service/read-more/' . $service->service_id);
+    }
+
+    // added function by frontend developer - nuhu
+    public function AboutUs(){
+        return view('consumer.themes.about_us');
+    }
+
+    public function previewTerms($language)
+    {
+        // Set the file name based on the language
+        if ($language === 'Sheria_na_Masharti') {
+            $fileName = 'Sheria_na_Masharti.pdf';
+        } else {
+            $fileName = 'Terms_and_Conditions_for_Reviewers.pdf';
+        }
+
+        $filePath = public_path("files/{$fileName}");
+
+        if (!file_exists($filePath)) {
+            abort(404, "File not found.");
+        }
+
+        return response()->file($filePath, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . $fileName . '"',
+        ]);
+    }
+
+    public function previewPrivacyPolicy($language)
+    {
+        // Set the file name based on the language
+        if ($language === 'Sera_ya_Faragha') {
+            $fileName = 'Sera_ya_Faragha.pdf';
+        } else {
+            $fileName = 'Privacy_Policy_for_Reviewers.pdf';
+        }
+
+        $filePath = public_path("files/{$fileName}");
+
+        if (!file_exists($filePath)) {
+            abort(404, "File not found.");
+        }
+
+        return response()->file($filePath, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="' . $fileName . '"',
+        ]);
+    }
+
+    public function aboutUsBusiness(){
+        return   view('business_fontend.about-us');
+    }
+
+    public function whyUsBusiness(){
+        return   view('business_fontend.why-us');
+    }
+    
+    
+    
+    public function contactBusiness(){
+        return   view('business_fontend.contact');
+    }
+
+
+    public function pricingBusiness(){
+        return view('business_fontend.pricing');
+    }
+    
+
+
+}
+>>>>>>> 82979f0 (category implemented successfully)
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 namespace App\Http\Controllers;
@@ -339,3 +340,346 @@ class LoginController extends Controller
         }
     }
 }
+=======
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ResetToken;
+use App\Models\Business;
+use App\Models\BusinessReview;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+// use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Illuminate\Support\Facades\Session;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use Illuminate\Support\Facades\Mail;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+
+        return view('auth.login');
+    }
+
+    public function processLogin(Request $request)
+    {
+        $rules = array(
+            'email' => 'required',
+            'password' => 'required',
+        );
+        $validator = Validator::make(request()->all(), $rules);
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator);
+        } else {
+            //process validation here
+            $credentials = array(
+                "email" => $request->email,
+                "password" => $request->password,
+            );
+
+            $user = Sentinel::authenticate($credentials);
+
+            if ($user) {
+
+                if ($user->inRole('business')) {
+                    return redirect('dashboard/business');
+                }
+                if ($user->inRole('consumer')) {
+                    // return Session::get('rating_input');
+                    // return redirect('consumer/evaluate/company');
+                    // $data = array(
+                    //     'rating_input'=> Session::get('rating_input'),
+                    //     'product_id'=> Session::get('product_id'),
+                    //     'review_input'=> Session::get('review_input'),
+                    // );
+
+
+
+                    if (Session::get('type') == 'business') {
+                        return redirect('consumer/business/write-review/' . Session::get('company_id'));
+                    }
+                    if (Session::get('type') == 'product') {
+                        return redirect('consumer/product/write-review/' . Session::get('product_id'));
+                    }
+                    if (Session::get('type') == 'service') {
+                        return redirect('consumer/service/write-review/' . Session::get('service_id'));
+                    }
+                }
+                return redirect('dashboard');
+            } else {
+                //return back
+                //                flash('Invalid email or password.')->error();
+                //return back
+                Session::flash('error', 'Invalid username or password!');
+                return redirect()->back()->withInput();
+            }
+        }
+    }
+
+    public function dashboard()
+    {
+
+        $businesses = Business::get();
+
+        $consumers = User::whereHas('roles', function ($query) {
+            $query->where('name', 'consumer');
+        })->get();
+
+        // $reviews = BusinessReview::get();
+
+        $ratings = Business::select('total_rating')->get();
+
+        $ratingsData = [
+            'Very good' => $ratings->where('total_rating', 5)->count(),
+            'Good' => $ratings->where('total_rating', 4)->count(),
+            'Okay' => $ratings->where('total_rating', 3)->count(),
+            'Bad' => $ratings->where('total_rating', 2)->count(),
+            'Terrible' => $ratings->where('total_rating', 1)->count(),
+        ];
+
+        $reviews = BusinessReview::with('user')->get();
+
+        $genderCounts = [
+            'Male' => 0,
+            'Female' => 0
+        ];
+
+        foreach ($reviews as $review) {
+            if ($review->user->gender === 'male') {
+                $genderCounts['Male']++;
+            } elseif ($review->user->gender === 'female') {
+                $genderCounts['Female']++;
+            }
+        }
+
+        $totalReviews = array_sum($genderCounts);
+
+       if ($totalReviews > 0) {
+        $genderPercentages = [
+            'Male' => ($genderCounts['Male'] / $totalReviews) * 100,
+            'Female' => ($genderCounts['Female'] / $totalReviews) * 100
+        ];
+        } else {
+            $genderPercentages = [
+                'Male' => 0,
+                'Female' => 0
+            ];
+        }
+
+
+        $topCompanies = Business::where('total_rating', '>', 2)
+            ->where('total_rating', '<=', 5)
+            ->orderBy('total_rating', 'DESC')
+            ->get();
+
+
+         $chartData = Category::has('business')
+            ->with(['business' => function ($query) {
+                $query->where('display', 1)
+                    ->where('total_rating', '>', 2) // Only businesses with rating greater than 2
+                    ->orderBy('total_rating', 'desc');
+            }])
+            ->get()
+            ->map(function ($category) {
+                // Get the top business, if any
+                $topBusiness = $category->business->first();
+
+                return [
+                    'name' => $topBusiness ? $topBusiness->business_name : $category->category_name, // Business name or category name
+                    'average' => $topBusiness ? (float) $topBusiness->total_rating : 0 // Rating or 0
+                ];
+            })
+            ->filter(function ($item) {
+                return $item['average'] > 0; // Ensure we only return businesses with ratings
+            });
+
+         // Filter categories to only include those with businesses
+            //        $chartData = $categories->filter(function ($category) {
+            //            return $category->business->isNotEmpty(); // Only keep categories with businesses
+            //        })->map(function ($category) {
+            //            $topBusiness = $category->business->first();
+            //            return [
+            //                'name' => $topBusiness->business_name, // Use business name for display
+            //                'average' => (float)$topBusiness->total_rating // Convert rating to float
+            //            ];
+            //        });
+
+            return view(
+                'admin.dashboard',
+                compact(
+                    'businesses',
+                    'consumers',
+                    'reviews',
+                    'topCompanies',
+                    'ratingsData',
+                    'genderPercentages',
+                    'chartData'
+                )
+            );
+    }
+
+
+    public function loginforBusiness()
+    {
+
+        return view('owner.auth.login');
+    }
+
+    public function postloginforBusiness()
+    {
+        $rules = array(
+            'email' => 'required',
+            'password' => 'required',
+        );
+        $validator = Validator::make(request()->all(), $rules);
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator);
+        } else {
+            //process validation here
+            $credentials = array(
+                "email" => $request->email,
+                "password" => $request->password,
+            );
+
+            $user = Sentinel::authenticate($credentials);
+
+
+            if ($user) {
+
+                if ($user->inRole('business')) {
+                    return redirect('dashboard/business');
+                }
+
+                return redirect('dashboard');
+            } else {
+                //return back
+                //                flash('Invalid email or password.')->error();
+                //return back
+                Session::flash('error', 'Invalid username or password.!');
+                return redirect()->back()->withInput();
+            }
+        }
+    }
+
+    public function consumerLogin()
+    {
+
+        return view('consumer.user.login');
+    }
+
+    public function postConsumerLogin(Request $request)
+    {
+        $rules = array(
+            'email' => 'required',
+            'password' => 'required',
+        );
+        $validator = Validator::make(request()->all(), $rules);
+        if ($validator->fails()) {
+            return redirect()->back()->withInput()->withErrors($validator);
+        } else {
+            //process validation here
+            $credentials = array(
+                "email" => $request->email,
+                "password" => $request->password,
+            );
+
+            $user = Sentinel::authenticate($credentials);
+
+            if ($user) {
+                if ($user->inRole('consumer')) {
+                    if (Session::get('type') == 'business') {
+                        return redirect('consumer/business/write-review/' . Session::get('company_id'));
+                    }
+                    if (Session::get('type') == 'product') {
+                        return redirect('consumer/product/write-review/' . Session::get('product_id'));
+                    }
+                    if (Session::get('type') == 'service') {
+                        return redirect('consumer/service/write-review/' . Session::get('service_id'));
+                    }
+                }
+                if ($user->inRole('support')) {
+                    return redirect('dashboard');
+                }
+                return redirect('dashboard');
+            } else {
+                Session::flash('error', 'Invalid username or password.!');
+                return redirect()->back()->withInput();
+            }
+        }
+    }
+
+    public function logout()
+    {
+        Sentinel::logout(null, true);
+        // Artisan::call('cache:clear');
+        Session::forget(['rating_input', 'review_input', 'company_id', 'product_id', 'service_id']);
+        return redirect('/');
+    }
+
+    public function forgetPassword()
+    {
+        return view('auth.forgot');
+    }
+
+    public function forgetEmailCheck(Request $request)
+    {
+
+        // Validate the email input
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        // $token = random_int(10000, 99999);
+        if ($user) {
+            $user->save();
+
+            // Mail::to($user->email)->send(new ResetToken($user));
+
+            Session::flash('success', 'An email has been sent to ' . $request->email . '. Please check your inbox.');
+            return redirect()->back()->withInput();
+        } else {
+            Session::flash('error', "We can't find a user with that email address.");
+            return redirect()->back()->withInput();
+        }
+    }
+
+    public function passwordReset()
+    {
+        return view('auth.reset');
+    }
+
+    public function passwordResetCodeCheck(Request $request)
+    {
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user) {
+            if ($user->reset_status == 0) {
+                $resetCode = Sentinel::findById($user->id);
+
+                $credentials = [
+                    'reset_status' => 1,
+                    'password' => $request->newpassword
+                ];
+
+                $resetUser = Sentinel::update($resetCode, $credentials);
+
+                return redirect('/login');
+            }
+            if ($user->reset_status == 1) {
+                Session::flash('error', "This token already used");
+                return redirect('password/reset-with-code');
+            }
+        } else {
+            Session::flash('error', "This token not exist");
+            return redirect('password/reset-with-code');
+        }
+    }
+}
+>>>>>>> 82979f0 (category implemented successfully)
